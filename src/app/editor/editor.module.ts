dfadd1b54fd2332df4ab58906a3e778d4e1d7abe
mikiyas simeneh <mikiyassimeneh147@gmail.com>
2023-12-15 01:24:25 -1000

    design menu bar

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -5,16 +5,20 @@ import { PanelComponent } from './panel/panel.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { TextEditorComponent } from './text-editor/text-editor.component';
+import { OpenedProjectsComponent } from './opened-projects/opened-projects.component';
+import { EditorComponent } from './editor.component';
 
 @NgModule({
-  imports: [],
   declarations: [
+    EditorComponent,
     ActivityBarComponent,
     PrimarySideBarComponent,
     PanelComponent,
     StatusBarComponent,
     MenuBarComponent,
     TextEditorComponent,
+    OpenedProjectsComponent,
   ],
+  imports: [],
 })
 export class EditorModule {}
---
+++ src/app/editor/opened-projects/opened-projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opened-projects',
+  templateUrl: './opened-projects.component.html',
+  styleUrls: ['./opened-projects.component.scss']
+})
+export class OpenedProjectsComponent {
+
+}
